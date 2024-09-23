08be57d34eb401cc20fc227ef80b067bbbf94210
Hasangitt <pingpong038759@gmail.com>
2024-09-24 00:56:14 +0600

    class, this, dot, bracket

---
+++ class.js
@@ -0,0 +1,12 @@
+const products = [
+    {id: 1, name: 'hp', price: 65000},
+    {id: 2, name: 'dell', price: 15000},
+    {id: 3, name: 'mac', price: 165000},
+    {id: 4, name: 'lenevo', price: 45000},
+]
+
+// similar
+
+class product{
+    
+}
---
+++ dot.js
@@ -0,0 +1,10 @@
+const person ={
+    name: 'kodom ali vai',
+    job: 'badam beche',
+    3: 'third',
+    'add-ress': 'dhaka'
+}
+
+console.log(person.job)
+console.log(person['3'])
+console.log(person['add-ress'])
